cbb0a9243ed58fa261cbdce4cb5bc34136ee9b09
noenoenoe93 <79853812+noenoenoe93@users.noreply.github.com>
2021-11-10 16:20:06 +0100

    Update BD jura.sql

--- BD jura.sql
+++ BD jura.sql
@@ -6,4 +6,26 @@ CREATE TABLE Reservation (
    Prenom VARCHAR(30) NOT NULL,
    duree_sejour int NOT NULL,
    disponnibilite VARCHAR(10) NOT NULL,
-   paiement VARHAR(10);)
+   paiement VARCHAR(10);)
+
+
+CREATE TABLE Authentification(
+   ID_Client VARCHAR(50),
+   Nom VARCHAR(50),
+   Numero BIGINT,
+   Prenom VARCHAR(50),
+   Email VARCHAR(50),
+   PRIMARY KEY(ID_Client));
+
+
+CREATE TABLE Hebergement (
+   ID_Hebergement serial PRIMARY KEY,
+   Douche int NOT NULL,
+   Toilette int NOT NULL,
+   nombre_lits int NOT NULL,
+   Hebergement_lieu VARCHAR(255) NOT NULL,
+   balcon int NOT NULL,
+   lits_double int NOT NULL,
+   list_separes int NOT NULL,
+   cloisons int NOT NULL,
+   logement_handicape int NOT NULL);
