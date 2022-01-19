7d6a0351831242cffddf25269243d54b2dc900fd
BrandonUng77 <80267985+BrandonUng77@users.noreply.github.com>
2022-01-19 17:17:17 +0100

    Add files via upload

---
+++ Jura_Brandon/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    
+        <?php include('Header.php'); ?>
+    
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+        
+        <?php include('Footer.php'); ?>
+    </body>
+</html>
