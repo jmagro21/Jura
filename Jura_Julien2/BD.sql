c5d89bbde24b72727e666ab44182c71ad937b41f
jmagro21 <80267976+jmagro21@users.noreply.github.com>
2022-01-26 11:25:27 +0100

    Add files via upload

--- Jura_Julien2/BD.sql
+++ Jura_Julien2/BD.sql
@@ -1,1 +1,23 @@
-CREATE TABLE `jeu`.`utilisateur1` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `pseudo` TEXT NOT NULL , `mdp` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `jeu`.`utilisateur1` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT , 
+    `pseudo` TEXT NOT NULL ,
+    `mdp` TEXT NOT NULL ,
+    PRIMARY KEY (`id`)) 
+    ENGINE = InnoDB;
+
+CREATE TABLE `chambre` (
+  `id` int(11) NOT NULL,
+  `nom` text NOT NULL,
+  `lit` int(3) NOT NULL,
+  `descript` longtext NOT NULL,
+  `special` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  `id_chambre` int(11) NOT NULL,
+  `dated` date NOT NULL,
+  `datef` date NOT NULL,
+  `paye` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
