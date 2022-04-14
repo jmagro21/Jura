b3d9c1490e8b7cc590cfe82e27e8185f933cfc33
jmagro21 <80267976+jmagro21@users.noreply.github.com>
2022-04-14 20:07:29 +0200

    Delete Jura_Julien2 directory

--- Jura_Julien2/Footer.php
+++
@@ -1,4 +0,0 @@
-<footer id="bas_de_page">
-    <br />
-    <p>Copyright Groupe Noé/Brandon/Julien</p>
-</footer>
--- Jura_Julien2/Test.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('Nhotel.php');
-$ichambre = new Nhotel();
-$ichambre->Nhotel('Chambre triple','3','Chambre Spatieuse','1');
-
-?>
